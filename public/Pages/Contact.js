bef01ca832b453703266bc8cc398fc62434ee2d3
ms <shaheed_manzur@yahoo.com>
2021-08-16 13:45:31 -0400

    Contact and Projects

---
+++ public/Pages/Contact.js
@@ -0,0 +1,20 @@
+// contacts tab
+import Container from "react-bootstrap/Container";
+import Contact from "../../components/Contact";
+
+const ContactContainer = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-center">Contact</h1>
+      </div>
+      <Container>
+        <div className="p-3">
+          <Contact />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ContactContainer;
