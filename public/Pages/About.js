a17c1b0e776c168fb523193f1dfa453a70a8dd47
ms <shaheed_manzur@yahoo.com>
2021-08-16 13:43:17 -0400

    About.js

---
+++ public/Pages/About.js
@@ -0,0 +1,20 @@
+// about tab
+import Container from "react-bootstrap/Container";
+import About from "../components/About";
+
+const AboutContainer = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-center">About</h1>
+      </div>
+      <Container>
+        <div className="p-3">
+          <About />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default AboutContainer;
