57d2736c32e675c93264d97e4d5753e3cb005f27
ms <shaheed_manzur@yahoo.com>
2021-08-17 15:27:53 -0400

    email

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,6 +7,7 @@ import { Card, Container, Image } from "react-bootstrap";
 // images and pdf
 import github from "../../assets/images/github.png";
 import lnkd from "../../assets/images/lnkd.png";
+import email from "../../assets/images/email.png";
 import resume from "../../assets/images/resume.png";
 import me_resume from "../../assets/ms_2021.pdf";
 
@@ -17,6 +18,7 @@ const Footer = () => {
             <Card.Body className="text-center">
               <Card.Link href="https://www.linkedin.com/in/manzur-shaheed-9aa75926/" target="_blank"><Image src={lnkd} /></Card.Link>
               <Card.Link href="https://github.com/manzur-shaheed" target="_blank"><Image src={github} /></Card.Link>
+              <Card.Link href="mailto:shaheed_manzur@yahoo.com" target="_blank"><Image src={email} /></Card.Link>
               <Card.Link href={me_resume} target="_blank"><Image src={resume} /></Card.Link>
             </Card.Body>
       </Card>
